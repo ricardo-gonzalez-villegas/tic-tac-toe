39311a2837b06022132a2e8069440d6d2d03666d
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-02-22 22:23:58 +0900

    modules and factory functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+const form = document.getElementById("player-form");
+
+let playerOneName = "player1";
+let playerTwoName = "player2";
+
+const Player = name => {
+  "use strict";
+  let getName = name;
+
+  function consoleName() {
+    console.log("the name has been updated" + name);
+  }
+  return {
+    getName,
+    consoleName
+  };
+};
+
+const playerOne = Player(playerOneName, "o");
+const playerTwo = Player(playerTwoName, "x");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  playerOneName = form.querySelector('input[name="playerOne"]').value;
+  playerTwoName = form.querySelector('input[name="playerTwo"]').value;
+
+  if (playerOneName == "" || playerTwo == "") {
+    return;
+  }
+
+  playerOne.getName = playerOneName;
+  playerOne.consoleName();
+});
+
 const gameBoard = (function() {
   "use strict";
 
@@ -22,14 +57,8 @@ const displayController = (function() {
   return {};
 })();
 
-const Player = name => {
+const game = (function() {
   "use strict";
-  const getName = () => name;
 
-  return {
-    getName
-  };
-};
-
-const playerOne = Player("o");
-const playerTwo = Player("x");
+  return {};
+})();
